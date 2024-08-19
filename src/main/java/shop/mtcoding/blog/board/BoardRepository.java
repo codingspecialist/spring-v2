048395cf70810afa1eae49610471df87c96c2b00
getinthere <getinthere@naver.com>
2024-08-19 13:59:54 +0900

    게시글 목록 테스트 완료

--- src/main/java/shop/mtcoding/blog/board/BoardRepository.java
+++ src/main/java/shop/mtcoding/blog/board/BoardRepository.java
@@ -6,14 +6,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
 @Repository // @Repository를 붙이면 스프링이 new를 해서 IoC(컬렉션 List자료형 같은거) 에 저장한다.
 public class BoardRepository {
 
     @Autowired // IoC에 있는 객체를 찾아온다.
     private EntityManager em;
 
-    public BoardRepository() {
-        System.out.println("BoardRepository 생성자");
+    public List<Board> findAll() {
+        Query query = em.createNativeQuery("select * from board_tb order by id desc", Board.class);
+        List<Board> boardList = query.getResultList();
+        return boardList;
     }
 
     // insert 하기
--- src/test/java/shop/mtcoding/blog/board/BoardRepositoryTest.java
+++ src/test/java/shop/mtcoding/blog/board/BoardRepositoryTest.java
@@ -5,6 +5,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.Import;
 
+import java.util.List;
+
 //@SpringBootTest // C R e h2 -> 모든 레이어를 메모리에 다 올리고 테스트할 때 사용하는 어노테이션
 @DataJpaTest // h2, em
 @Import(BoardRepository.class) // br
@@ -12,6 +14,22 @@ public class BoardRepositoryTest {
 
     @Autowired
     private BoardRepository boardRepository;
+
+    @Test
+    public void findAll_test() {
+        // given
+
+        // when
+        List<Board> boardList = boardRepository.findAll();
+
+        // eye
+        System.out.println("사이즈 : " + boardList.size());
+        for (Board board : boardList) {
+            System.out.println(board.getTitle());
+            System.out.println(board.getContent());
+        }
+    }
+
 
     // 테스트 메서드에서는 매개변수를 사용할 수 없다.
     // 메서드명_test : 컨벤션
